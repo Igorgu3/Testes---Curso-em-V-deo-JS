a497bf010039a761c1b62839965c54e8f4b37706
Igor Colibri <fscigorgustavo@gmail.com>
2025-07-15 17:46:16 -0300

    Exercício Finalizado!

--- Aula 16/Ex. 5/script.js
+++ Aula 16/Ex. 5/script.js
@@ -1,60 +1,64 @@
 let num = [];
 let res = document.getElementById('res');
 
-function adicionar(){
+function adicionar() {
     res.innerHTML = '';
     var n = window.document.getElementById('numero');
     var numero1 = Number(n.value);
     let item = document.createElement('option');
-      
 
-    if(numero1 < 1 || numero1 > 100 || num.indexOf(numero1) != -1){
+
+    if (numero1 < 1 || numero1 > 100 || num.indexOf(numero1) != -1) {
         window.alert('Valor inválido ou já encontrado na lista!');
     }
 
-    else{
+    else {
         num.push(numero1);
         item.text = `Valor ${numero1} adicionado`;
         item.value = `tab${numero1}`;
         tab.appendChild(item);
-    } 
+    }
+
+    n.value = '';
+    n.focus();
 }
 
-function finalizar(){ 
+function finalizar() {
     let tamanho = num.length;
-    let menor = 1000;
-    let maior = 0;
+    let menor = num[0];
+    let maior = num[0];
     let soma = 0;
     let media = 0;
 
-    //menor
-    for (var i = 0; i < tamanho; i++){
-        if(num[i]<menor){
-            menor = num[i];
+    if (tamanho == 0) {
+        window.alert('Adicione valores antes de finalizar!');
+    }
+
+    else {
+
+        for (let pos in num) {
+            soma += num[pos];
+            if (num[pos] < menor) {
+                menor = num[pos];
+            }
+
+            if (num[pos] > maior){
+                maior = num[pos];
+            }
+            
         }
+
+        //média
+        media = soma / tamanho;
+
+        res.innerHTML += `Ao todo, temos ${tamanho} números cadastrados. <br>
+        O maior valor informado foi ${maior}. <br>
+        O menor valor informado foi ${menor}. <br>
+        Somando todos os valores, temos ${soma}. <br>
+        A média dos valores digitados é ${media}.
+        `;
     }
 
-    //maior
-    for (var m = 0; m < tamanho; m++){
-        if(num[m]>maior){
-            maior = num[m];
-        }
-    }
-
-    //soma
-    for (var s = 0; s < tamanho; s++){
-        soma += num[s];
-    }
-
-    //média
-    media = soma/tamanho;
-
-    res.innerHTML += `Ao todo, temos ${tamanho} números cadastrados. <br>
-    O maior valor informado foi ${maior}. <br>
-    O menor valor informado foi ${menor}. <br>
-    Somando todos os valores, temos ${soma}. <br>
-    A média dos valores digitados é ${media}.
-    `;
 }
 
 
